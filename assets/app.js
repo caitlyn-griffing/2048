a3e6441e36bf0e02f80544dabac1687a23c2730f
Caitlyn Griffing <caitlyn.griffing@gmail.com>
2022-03-24 19:27:46 -0500

    created individual squares with 0

--- assets/app.js
+++ assets/app.js
@@ -2,12 +2,24 @@ document.addEventListener('DOMContentLoaded', () => {
     const gridDisplay = document.querySelector('.grid')
     const scoreDisplay = document.querySelector('#score')
     const resultDisplay = document.querySelector('#result')
+    const width = 4;
+    let squares = []
+
+    //create playing board
+    function createBoard() {
+        // loop until you reach 16 squares (4 squares by 4 squares)
+        // as long as i is less than 16 run this code
+        for(let i = 0; i < width*width; i++) {
+            square = document.createElement('div')
+            square.innerHTML = 0
+            gridDisplay.appendChild(square)
+            squares.push(square)
+        }
+    }
+
+    createBoard()
+
 })
 
-//create playing board
-function createBoard() {
-    // loop until you reach 16 squares (4 squares by 4 squares)
-    for(let i = 0; i < width*width; i++) {
-        
-    }
-}
+
+
