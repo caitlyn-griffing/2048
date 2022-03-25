7cce9062183f928d2a2bd9f796895b7f703b1222
Caitlyn Griffing <caitlyn.griffing@gmail.com>
2022-03-24 19:20:24 -0500

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 2048
+
+> If you are not willing to learn, no one can help you. If you are determined to learn, no one can stop you. - Zig Ziglar
---
+++ assets/app.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const gridDisplay = document.querySelector('.grid')
+    const scoreDisplay = document.querySelector('#score')
+    const resultDisplay = document.querySelector('#result')
+})
+
+//create playing board
+function createBoard() {
+    // loop until you reach 16 squares (4 squares by 4 squares)
+    for(let i = 0; i < width*width; i++) {
+        
+    }
+}
